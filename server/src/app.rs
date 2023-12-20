987f7c75cdc4684cd4fe68e74aae126f4346c1b5
hanifanrn <hanifanrizki@gmail.com>
2023-12-21 04:06:51 +0700

    restructure code

--- server/src/app.rs
+++ server/src/app.rs
@@ -1,6 +1,7 @@
 use crate::features::hello_world::hello_world_controller;
 
 use axum::Router;
+use tokio::net::TcpListener;
 
 pub fn routes_all() -> Router {
     return Router::new().merge(hello_world_controller::create_routes());
@@ -10,8 +11,7 @@ pub async fn run() {
     // build out application with a single route
     let app = routes_all();
     // run it with hyper on localhost::3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
